e9928f84f8697a14569ad69c779f6c9dc127cd70
Sam <xiaopeng.luo@mail.utoronto.ca>
2022-10-23 13:51:51 -0400

    update

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('hello')
+const express = require('express');
+
+const app = express();
+
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello</h1>');
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
